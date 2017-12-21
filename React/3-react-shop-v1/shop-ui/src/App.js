16fe650d9d1cb485c6b8b4bc926b8f504a86b0d0
nagabhushanamn <nagabhushanamn@gmail.com>
2017-12-21 12:00:06 +0530

    shop v1

--- React/3-react-shop-v1/shop-ui/src/App.js
+++ React/3-react-shop-v1/shop-ui/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import classnames from 'classnames';
 // import logo from './logo.svg';
 // import './App.css';
 
@@ -7,6 +8,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      tab: 1,
       products: [
         {
           name: 'Laptop',
@@ -26,15 +28,40 @@ class App extends Component {
     };
   }
 
+  handleTabChange(tab) {
+    this.setState({
+      tab
+    });
+  }
+
   renderBuyBtn(product) {
     if (product.canBuy)
       return <button className="btn btn-sm btn-primary">buy</button>
     else
       return null;
   }
+  renderTabPanel(product) {
+    let { tab } = this.state;
+    let panel;
+    switch (tab) {
+      case 1:
+        panel = <div className="panel"><p>{product.description}</p></div>
+        break;
+      case 2:
+        panel = <div className="panel"><p>Not Yet</p></div>
+        break;
+      case 3:
+        panel = <div className="panel"><p>None yet</p></div>
+        break;
+      default:
+        panel = null;
+
+    }
+    return panel;
+  }
 
   renderProducts() {
-    let { products } = this.state;
+    let { products, tab } = this.state;
     return products.map((product, idx) => {
       return (
         <div key={idx} className="list-group-item">
@@ -46,8 +73,16 @@ class App extends Component {
               <span className="badge">{idx + 1}</span>
               <h4>{product.name}</h4>
               <h5>&#8377;{product.price}</h5>
-              <p>{product.description}</p>
               {this.renderBuyBtn(product)}
+              <hr />
+              <section>
+                <ul className="nav nav-tabs">
+                  <li className={tab === 1 ? 'active' : ''}><a onClick={() => { this.handleTabChange(1) }}>Description</a></li>
+                  <li className={classnames({ active: tab === 2, other: true })}><a onClick={() => { this.handleTabChange(2) }}>Specification</a></li>
+                  <li className={classnames({ active: tab === 3, other: false })}><a onClick={() => { this.handleTabChange(3) }}>Reviews</a></li>
+                </ul>
+                {this.renderTabPanel(product)}
+              </section>
             </div>
           </div>
         </div>
@@ -57,9 +92,13 @@ class App extends Component {
 
 
   render() {
+    console.log('App :: render()');
     return (
       <div>
-        <nav className="navbar navbar-default">
+
+        {
+          /* 
+          <nav className="navbar navbar-default">
           <div className="container-fluid">
             <div className="navbar-header">
               <a className="navbar-brand" href="#">shopIT</a>
@@ -68,8 +107,12 @@ class App extends Component {
               <li><a href="#"><i className="glyphicon glyphicon-home"></i>Home</a></li>
             </ul>
           </div>
-        </nav>
+        </nav> 
+        */
+        }
+
         <div className="container">
+          <div className="page-header"> shopIT </div>
           <div className="list-group">
             {this.renderProducts()}
           </div>
